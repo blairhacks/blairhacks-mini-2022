c7780bb2a2260ec647bd716e55a5da1292f2b1cf
Tinu Vanapamula <tinuvanapamula@gmail.com>
2022-11-06 20:15:26 -0500

    change text to black

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,7 +90,7 @@ export default function Home() {
 							target="_blank" rel="noreferrer noopener"
 							className="self-start"
 						>
-							<div className="px-4 py-2 bg-emerald-500 rounded-xl text-2xl font-extrabold mt-6">
+							<div className="px-4 py-2 bg-emerald-500 rounded-xl text-black text-2xl font-extrabold mt-6">
 								Register (free!)
 							</div>
 						</a>
